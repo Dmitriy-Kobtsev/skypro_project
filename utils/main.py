cd0d72c1bd2711fb0a1776a20aa387a59755b228
Dmitriy-Kobtsev <kibtsev94@gmail.com>
2023-10-31 18:18:52 +0300

    add test

--- utils/main.py
+++ utils/main.py
@@ -29,9 +29,9 @@ def get_executed_operations(all_operations):
     """
     ex_operations = []
     for operation in all_operations:
-        if not (not operation):
-            if operation['state'] == 'EXECUTED':
-                ex_operations.append(operation)
+        if (not (not operation)) and (operation['state'] == 'EXECUTED'):
+            ex_operations.append(operation)
+
     ex_operations = sorted(
         ex_operations,
         key=lambda x: datetime.strptime(x['date'], '%Y-%m-%dT%H:%M:%S.%f'), reverse=True
@@ -39,15 +39,19 @@ def get_executed_operations(all_operations):
     return ex_operations
 
 
-def print_operations(ex_operations: list):
+def list_operations(ex_operations: list):
     """
-    Функция вывода пяти последних выполненных операций
+    Функция формирования списка пяти последних выполненных операций
     :param ex_operations: список выполненных операций
-    :return: пять последних выполненных операций
+    :return: список пяти последних выполненных операций
     """
+    if len(ex_operations) < 5:
+        error = 'количество выполненных операций меньше 5!'
+        return error
+
+    operations_list = []
     for ex_operation in ex_operations[:5]:
         date = datetime.strptime(f"{ex_operation['date']}", '%Y-%m-%dT%H:%M:%S.%f')
-        print(f'{date.strftime("%d.%m.%Y")} {ex_operation["description"]}')
 
         if "from" in ex_operation:
             operation = ex_operation['from'].split()
@@ -71,5 +75,17 @@ def print_operations(ex_operations: list):
             secret_count = get_mask_account(operation[-1])
             operation[-1] = secret_count
             _to = ' '.join(operation)
-        print(f'{_from} -> {_to}')
-        print(f'{ex_operation["operationAmount"]["amount"]} {ex_operation["operationAmount"]["currency"]["name"]}'+"\n")
+        operations_list.append(
+            {'date': date.strftime("%d.%m.%Y"),
+             'description': ex_operation['description'],
+             'from': _from, 'to': _to,
+             'amount': ex_operation["operationAmount"]["amount"],
+             'currency': ex_operation["operationAmount"]["currency"]["name"]
+             }
+        )
+    return operations_list
+
+
+def print_list(ex_op_list):
+    for ex_op in ex_op_list:
+        print(f'{ex_op["date"]} {ex_op["description"]}\n{ex_op["from"]}->{ex_op["to"]}\n{ex_op["amount"]} {ex_op["currency"]}\n')
